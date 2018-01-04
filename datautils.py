564c5ef24446178c2189e91d947e32a7c4a1ce1d
Thomas <34484027+hibididibidi@users.noreply.github.com>
2018-01-04 12:12:15 -0400

    Update and rename merge_all.py to datautils.py

--- datautils.py
+++ datautils.py
@@ -19,3 +19,25 @@ def merge_all(Curr=Curr,Bonds=Bonds,OilN=OilN,NetSp=NetSp,FundsRates=FundsRates,
     Ymag=Ymag.values.reshape(1,Feed.shape[1])
     return Feed, Y, Ymag, Feedt.iloc[:,-curtestex:] #Defaulting using last 220 full examples as predictor input, can be adjusted later
 
+def SplitData3way(X,Y,percent_train=90):
+    #First cut out the recent test and dev sets: all values in training set are prior to values in the test and dev
+    ntrain=int(percent_train/100*X.shape[1])
+    Permuttrain=list(np.random.permutation(ntrain))
+    Xtrain=np.float32(X[:,Permuttrain])
+    Ytrain=np.float32(Y[:,Permuttrain])
+    m=X.shape[1]
+#     X=(X-X.mean(axis=1).reshape(-1,1))/X.std(axis=1).reshape(-1,1) ###### Batch normalization did not seem to have any effect
+    Permuttestdev= list(np.random.permutation(m-ntrain)+ntrain)
+#     print(Permuttestdev)
+    ntest=len(Permuttestdev)//2
+#     print(ntest)
+    Permuttest=Permuttestdev[:ntest]
+    Permutdev=Permuttestdev[ntest:]
+    Xtest=np.float32(X[:,Permuttest])
+    Ytest=np.float32(Y[:,Permuttest])
+    Xdev=np.float32(X[:,Permutdev])
+    Ydev=np.float32(Y[:,Permutdev])
+#     print(Xtrain, Ytrain, Xtest, Ytest, Xdev)
+    return Xtrain, Ytrain, Xtest, Ytest, Xdev, Ydev
+
+
